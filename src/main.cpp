45cb66dd4c7ad2d3ade653708aadd16294407524
Rohan <dev.creator42@gmail.com>
2024-10-23 16:19:01 -0700

    cout

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,21 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
-
+#include <iostream>
+#include <fstream>  
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
 vex::brain       Brain;
 
 // define your global instances of motors and other devices here
+distance rightSensor = distance(PORT11);
+distance leftSensor = distance(PORT18);
+
 
 
 int main() {
 
-    Brain.Screen.printAt( 10, 50, "Hello V5" );
-   
-    while(1) {
-        
-        // Allow other tasks to run
-        this_thread::sleep_for(10);
-    }
+    std::cout << "sigma string";
+
 }
